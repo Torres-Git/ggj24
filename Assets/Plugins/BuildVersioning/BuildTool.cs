c680d2c3c895b86a45fbb235f11b18b1d895078d
Torres-Git <luismigueltorres@outlook.pt>
2024-01-26 08:38:58 +0000

    initial setup

---
+++ Assets/Plugins/BuildVersioning/BuildTool.cs
@@ -0,0 +1,67 @@
+﻿/* MIT License
+Copyright (c) 2016 RedBlueGames
+*/
+
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class BuildTool : MonoBehaviour
+{
+	private const string GAME_NAME = "GGJ24_LettuceStudios";
+	[MenuItem("Build/Log Version")]
+	public static string Log()
+	{
+		var v = Git.BuildVersion;
+		Debug.Log("Version: " + v);
+		return v;
+	}
+	
+	[MenuItem("Build/Build with Version")]
+	public static void Build()
+	{
+		string buildVersion = Git.BuildVersion;
+		
+		if(string.IsNullOrEmpty(buildVersion))
+		{
+			Debug.LogError("Error when retriving git version");
+			return;
+		}
+		
+		PlayerSettings.bundleVersion = buildVersion;
+
+		string[] scenesToBuild = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes);
+		BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+		
+		string buildPath = 
+			"Builds" + Path.DirectorySeparatorChar 
+			+ buildTarget + Path.DirectorySeparatorChar 
+			+ PlayerSettings.bundleVersion + Path.DirectorySeparatorChar 
+			+ GAME_NAME;
+			
+		BuildOptions buildOptions = BuildOptions.None;
+
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+		{
+			scenes = scenesToBuild,
+			locationPathName = buildPath,
+			target = buildTarget,
+			options = buildOptions
+		};
+		
+
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log("Build failed");
+		}
+	}
+}
---
+++ Assets/ggj24/_Scripts/Audio/AudioEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Serrot.Audio
+{
+public abstract class AudioEvent : ScriptableObject
+{
+	public abstract void Play(AudioSource source);
+}
+}
